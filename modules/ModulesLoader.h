c10371bc179ab11fe79bc9e08a413bff7da1b012
lee <leelf_00@126.com>
2024-11-24 09:58:32 +0800

    [Core/Unit] Fix a crash by taken damage. (#379)

---
+++ modules/ModulesLoader.h
@@ -0,0 +1,13 @@
+// ModulesLoader.h
+#ifndef MODULESLOADER_H
+#define MODULESLOADER_H
+
+#include "Define.h"
+#include "ScriptMgr.h"
+
+extern void AddModulesScripts();
+
+#endif // MODULESLOADER_H
+
+// ModulesLoader.cpp.in.cmake
+// Ce fichier est généré automatiquement par CMake
